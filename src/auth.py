20c155add179be07b492de770b9e4d2459220ca7
balashivam <balashivam5555@gmail.com>
2026-05-26 12:50:30 +0530

    authentication service

---
+++ src/auth.py
@@ -0,0 +1,18 @@
+from datetime import timedelta,datetime
+from jose import jwt,JWTError
+from passlib.context import CryptContext
+from .config import secret_key,algo,time_to_expire
+
+pwd_context = CryptContext( schemes=["argon2"],deprecated = "auto")
+
+def hash_password(password:str):
+    return pwd_context.hash(password)
+def verify_password(plain_passwd,hashed_passwd):
+    return pwd_context.verify(plain_passwd,hashed_passwd)
+def create_access_token(data:dict):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow()+timedelta(min = time_to_expire)
+    to_encode.update({"expire":expire})
+    encoded_jwt = jwt.encode(to_encode,key=secret_key,algorithm=algo)
+    return encoded_jwt
